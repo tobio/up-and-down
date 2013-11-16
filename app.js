ea0e8068858984a78132a021d81a1ecd36264c24
Toby Brain <tobyb@freshview.com>
2013-11-16 22:01:09 +1100

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
+app.use(express.logger());
+
+app.get('/', function(req, res) {
+    res.render('master');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});

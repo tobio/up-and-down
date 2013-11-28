71391714df0fe8706c9b95176fef87c9ec3dca42
Toby Brain <tobyb@freshview.com>
2013-11-29 08:54:55 +1100

    UI for selecting a scoring system

--- public/js/ctrl/newGame.js
+++ public/js/ctrl/newGame.js
@@ -2,6 +2,12 @@ define(['../upanddown', '../dir/player'], function(module) {
     module.controller('NewGameCtrl', ['$scope', function($scope) {
         $scope.players = []; 
         $scope.blank = {};
+        $scope.scoringSystems = [{ // Get this list from a provider
+            name: 'Basic'
+        }, {
+            name: 'Ben\'s scoring'
+        }];
+        $scope.selectedScoringSystem = $scope.scoringSystems[0];
         
         $scope.add = function(player) {
             $scope.blank = {};
